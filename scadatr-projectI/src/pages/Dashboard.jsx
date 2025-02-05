c9d829b3a0c0e5a02539e79d2c3efde6cc852bd2
Daniel <dfrodrigues23@gmail.com>
2025-02-05 23:13:37 +0000

    dash incomplete

--- scadatr-projectI/src/pages/Dashboard.jsx
+++ scadatr-projectI/src/pages/Dashboard.jsx
@@ -1,10 +1,22 @@
+import React from 'react';
+import './Dashboard.css'; // Import the CSS file
 
 const Dashboard = () => {
-    return (
-        <div className="Dashboard">
-            <h1>Dashboard</h1>
-        </div>
-    );
+  return (
+    <div className="Dashboard">
+      <div className="row">
+        <div className="rectangle large"></div>
+      </div>
+      <div className="row">
+        <div className="rectangle small"></div>
+        <div className="rectangle small"></div>
+        <div className="rectangle small"></div>
+      </div>
+      <div className="row">
+        <div className="rectangle large"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
