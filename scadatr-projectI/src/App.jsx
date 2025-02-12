a3d4a122c7c69549bbf03d06ba717b8a0a02cb79
Daniel <dfrodrigues23@gmail.com>
2025-02-12 19:58:47 +0000

    Creating Grahps

--- scadatr-projectI/src/App.jsx
+++ scadatr-projectI/src/App.jsx
@@ -8,10 +8,10 @@ import Contacts from "./scenes/contacts";
 import Invoices from "./scenes/invoices";
 import Form from "./scenes/form";
 import FAQ from "./scenes/faq";
-// import Bar from "./scenes/bar";
-// import Line from "./scenes/line";
-// import Pie from "./scenes/pie";
-// import Geography from "./scenes/geography";
+import Bar from "./scenes/bar";
+import Pie from "./scenes/pie";
+import Line from "./scenes/line";
+
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
 
@@ -34,11 +34,9 @@ function App() {
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
               <Route path="/faq" element={<FAQ />} />
-              {/*
               <Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
-              <Route path="/line" element={<Line />} />                            
-              <Route path="/geography" element={<Geography />} /> */}
+              <Route path="/line" element={<Line />} />
             </Routes>
           </main>
         </div>
