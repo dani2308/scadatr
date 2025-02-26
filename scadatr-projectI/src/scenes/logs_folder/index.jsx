4bc05e8ebc0a552d4421e511f9325d66bef35db0
Daniel <dfrodrigues23@gmail.com>
2025-02-26 18:23:03 +0000

    folder Logs Corrected

---
+++ scadatr-projectI/src/scenes/logs_folder/index.jsx
@@ -0,0 +1,72 @@
+import { Box, Typography, useTheme } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import { tokens } from "../../theme";
+import { mockDataInvoices } from "../../data/mockData";
+import Header from "../../components/Header";
+
+const Logs = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const columns = [
+    {
+      field: "tipo",
+      headerName: "Tipo",
+      flex: 1,
+      cellClassName: "name-column--cell",
+    },
+    {
+      field: "origem",
+      headerName: "Origem",
+      flex: 1,
+    },
+    {
+      field: "date_hour",
+      headerName: "Data/Hora",
+      flex: 1,
+    },
+    {
+      field: "description",
+      headerName: "Descrição",
+      flex: 1,
+    },
+  ];
+
+  return (
+    <Box m="20px">
+      <Header title="LOGS" subtitle="Tabela de Logs Captados pelo Sistema" />
+      <Box
+        m="40px 0 0 0"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .name-column--cell": {
+            color: colors.grey[100],
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: colors.blueAccent[700],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: colors.primary[400],
+          },
+          "& .MuiDataGrid-footerContainer": {
+            borderTop: "none",
+            backgroundColor: colors.blueAccent[700],
+          },
+          "& .MuiCheckbox-root": {
+            color: `${colors.grey[100]} !important`,
+          },
+        }}
+      >
+        <DataGrid rows={mockDataInvoices} columns={columns} />
+      </Box>
+    </Box>
+  );
+};
+
+export default Logs;
