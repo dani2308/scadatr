0d0192c80341f7be246c5eb95746306b20fdfd8a
Daniel <dfrodrigues23@gmail.com>
2025-04-24 00:08:45 +0100

    dataset changes

--- backend/app/ml_model.py
+++ backend/app/ml_model.py
@@ -13,18 +13,19 @@ scaler = joblib.load(SCALER_PATH)
 
 # Lista de features usada durante o treino (ordem correta)
 FEATURE_COLUMNS = [
-    "Destination Port", "Flow Duration", "Total Fwd Packets", "Total Length of Fwd Packets",
-    "Fwd Packet Length Max", "Fwd Packet Length Min", "Fwd Packet Length Mean", "Fwd Packet Length Std",
-    "Bwd Packet Length Max", "Bwd Packet Length Min", "Bwd Packet Length Mean", "Bwd Packet Length Std",
-    "Flow Bytes/s", "Flow Packets/s", "Flow IAT Mean", "Flow IAT Std", "Flow IAT Max", "Flow IAT Min",
-    "Fwd IAT Total", "Fwd IAT Mean", "Fwd IAT Std", "Fwd IAT Max", "Fwd IAT Min",
-    "Bwd IAT Total", "Bwd IAT Mean", "Bwd IAT Std", "Bwd IAT Max", "Bwd IAT Min",
-    "Fwd Header Length", "Bwd Header Length", "Fwd Packets/s", "Bwd Packets/s",
-    "Min Packet Length", "Max Packet Length", "Packet Length Mean", "Packet Length Std", "Packet Length Variance",
-    "FIN Flag Count", "PSH Flag Count", "ACK Flag Count", "Average Packet Size",
-    "Subflow Fwd Bytes", "Init_Win_bytes_forward", "Init_Win_bytes_backward", "act_data_pkt_fwd",
-    "min_seg_size_forward", "Active Mean", "Active Max", "Active Min", "Idle Mean", "Idle Max", "Idle Min"
+    "Destination_Port", "Flow_Duration", "Total_Fwd_Packets", "Total_Length_of_Fwd_Packets",
+    "Fwd_Packet_Length_Max", "Fwd_Packet_Length_Min", "Fwd_Packet_Length_Mean", "Fwd_Packet_Length_Std",
+    "Bwd_Packet_Length_Max", "Bwd_Packet_Length_Min", "Bwd_Packet_Length_Mean", "Bwd_Packet_Length_Std",
+    "Flow_Bytes/s", "Flow_Packets/s", "Flow_IAT_Mean", "Flow_IAT_Std", "Flow_IAT_Max", "Flow_IAT_Min",
+    "Fwd_IAT_Total", "Fwd_IAT_Mean", "Fwd_IAT_Std", "Fwd_IAT_Max", "Fwd_IAT_Min",
+    "Bwd_IAT_Total", "Bwd_IAT_Mean", "Bwd_IAT_Std", "Bwd_IAT_Max", "Bwd_IAT_Min",
+    "Fwd_Header_Length", "Bwd_Header_Length", "Fwd_Packets/s", "Bwd_Packets/s",
+    "Min_Packet_Length", "Max_Packet_Length", "Packet_Length_Mean", "Packet_Length_Std", "Packet_Length_Variance",
+    "FIN_Flag_Count", "PSH_Flag_Count", "ACK_Flag_Count", "Average_Packet_Size",
+    "Subflow_Fwd_Bytes", "Init_Win_bytes_forward", "Init_Win_bytes_backward", "act_data_pkt_fwd",
+    "min_seg_size_forward", "Active_Mean", "Active_Max", "Active_Min", "Idle_Mean", "Idle_Max", "Idle_Min"
 ]
+
 
 def predict_traffic(data_dict: dict) -> str:
     try:
