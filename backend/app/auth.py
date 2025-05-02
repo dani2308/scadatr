6425a023d4c1e1c421e7ba39465d1b948a5b607c
Daniel <dfrodrigues23@gmail.com>
2025-05-02 12:36:16 +0100

    requirements.txt update

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -1,24 +1,27 @@
+# Biblioitecas para Config de JWT e Hashing
 from datetime import datetime, timedelta
-from jose import JWTError, jwt
-from passlib.context import CryptContext
-from sqlalchemy.orm import Session
+from jose import JWTError, jwt # Biblioteca para criar e verificar JWT Tokens
+from passlib.context import CryptContext # Biblioteca para hashing de passwords
+
+# Bibliotecas para Segurança com FastAPI
+from sqlalchemy.orm import Session 
 from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordBearer # Middleware de segurança que extrai o token JWT
 
 from . import models, database
 
-# Chave secreta para codificação JWT (deve ser guardada em ambiente seguro em produção)
+# Chave para codificação JWT
 SECRET_KEY = "segredo_deve_ser_mudado_no_futuro"
-ALGORITHM = "HS256"
+ALGORITHM = "HS256" # Algoritmo de Encriptação JWT
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
-# Contexto para hashing de passwords
+# Definição do algorimto de hashing (bcrypt)
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-# OAuth2 schema
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")  # Garante que este caminho corresponde ao teu endpoint de login
+# Fluxo do OAuth2 para autenticação com bearer token (/login gera token)
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login") 
 
-# Verifica a password em texto plano com a password hashed
+# Verifica a password introduzida com o hash da password
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
@@ -26,14 +29,14 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
 def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
 
-# Autentica o utilizador através do email e password
+# Autentica o utilizador através do email e password (via Session)
 def authenticate_user(db: Session, email: str, password: str):
     user = db.query(models.User).filter(models.User.email == email).first()
     if not user or not verify_password(password, user.password_hash):
         return False
     return user
 
-# Cria um token JWT
+# Cria um token JWT e encripta-o
 def create_access_token(data: dict, expires_delta: timedelta | None = None) -> str:
     to_encode = data.copy()
     expire = datetime.now() + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
